63bfda95eca931469ffe0b7a9049fcfb2acce308
Gerrett84 <Gerrett84@gmail.com>
2014-01-19 10:13:02 +0800

    Lock Screen - Add Lockscreen Wallpaper (2/2) PS3 add Default Wallpaper as option
    PS4 fix preference list doesnt show selected item
    
    PS7:
    Rebased and modified to check for existence of a lock screen
    wallpaper when updating the preference.
    
    Change-Id: I2f4db0ddc132482d191eea4f76160b51b4063a60

--- src/com/android/settings/temasek/ScreenAndAnimations.java
+++ src/com/android/settings/temasek/ScreenAndAnimations.java
@@ -1,8 +1,22 @@
 package com.android.settings.temasek;
 
+import java.io.File;
+import java.io.IOException;
+
+import android.app.Activity;
 import android.app.ActivityManager;
+import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.Rect;
+import android.net.Uri;
+
+
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.provider.Settings;
@@ -12,6 +26,10 @@ import android.preference.Preference;
 import android.preference.PreferenceScreen;
 import android.preference.PreferenceCategory;
 import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.MediaStore;
+import android.view.Display;
+import android.view.Window;
+import android.widget.Toast;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -19,14 +37,25 @@ import com.android.settings.SettingsPreferenceFragment;
 public class ScreenAndAnimations extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
+    private static final int REQUEST_CODE_BG_WALLPAPER = 1024;
+    private static final int LOCKSCREEN_WALLPAPER_CUSTOM = 0;
+    private static final int LOCKSCREEN_DEFAULT_WALLPAPER = 1;
+
     private static final String KEY_ANIMATION_OPTIONS = "category_animation_options";
     private static final String KEY_POWER_CRT_MODE = "system_power_crt_mode";
     private static final String KEY_LISTVIEW_ANIMATION = "listview_animation";
     private static final String KEY_LISTVIEW_INTERPOLATOR = "listview_interpolator";
+    private static final String KEY_LOCKSCREEN_WALLPAPER = "default_lock_wallpaper";
+    private static final String WALLPAPER_IMAGE_PATH  = "/data/system/theme/wallpaper";
+    private static final String WALLPAPER_FILE_NAME = "default_lock_wallpaper.jpg";
 
+    private ListPreference mCustomWallpaper;
     private ListPreference mCrtMode;
     private ListPreference mListViewAnimation;
     private ListPreference mListViewInterpolator;
+
+    private File mWallpaperImage;
+    private File mWallpaperTemporary;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -67,11 +96,53 @@ public class ScreenAndAnimations extends SettingsPreferenceFragment implements
         mListViewInterpolator.setValue(String.valueOf(listviewinterpolator));
         mListViewInterpolator.setSummary(mListViewInterpolator.getEntry());
         mListViewInterpolator.setOnPreferenceChangeListener(this);
+
+        //Custom Lockscreen wallpaper
+        mCustomWallpaper = (ListPreference) findPreference(KEY_LOCKSCREEN_WALLPAPER);
+        mCustomWallpaper.setOnPreferenceChangeListener(this);
+        updateCustomWallpaperSummary();
+
+        mWallpaperImage = new File(WALLPAPER_IMAGE_PATH + "/default_lock_wallpaper.jpg");
+        mWallpaperTemporary = new File(WALLPAPER_IMAGE_PATH + "/default_lock_wallaper.tmp");
+    }
+
+    private void updateCustomWallpaperSummary() {
+        int resId;
+        if (!lockWallpaperExists()) {
+            resId = R.string.lockscreen_default_wallpaper;
+            mCustomWallpaper.setValueIndex(LOCKSCREEN_DEFAULT_WALLPAPER);
+        } else {
+            resId = R.string.lockscreen_wallpaper_custom;
+            mCustomWallpaper.setValueIndex(LOCKSCREEN_WALLPAPER_CUSTOM);
+        }
+        mCustomWallpaper.setSummary(getResources().getString(resId));
     }
 
     @Override
     public void onResume() {
         super.onResume();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE_BG_WALLPAPER) {
+            int hintId;
+
+            if (resultCode == Activity.RESULT_OK) {
+                if (mWallpaperTemporary.exists()) {
+                    mWallpaperTemporary.renameTo(mWallpaperImage);
+                }
+                hintId = R.string.lockscreen_wallpaper_result_successful;
+                updateCustomWallpaperSummary();
+            } else {
+                if (mWallpaperTemporary.exists()) {
+                    mWallpaperTemporary.delete();
+                }
+                hintId = R.string.lockscreen_wallpaper_result_not_successful;
+            }
+            Toast.makeText(getActivity(),
+                    getResources().getString(hintId), Toast.LENGTH_LONG).show();
+        }
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
@@ -101,7 +172,63 @@ public class ScreenAndAnimations extends SettingsPreferenceFragment implements
                     listviewinterpolator);
             mListViewInterpolator.setSummary(mListViewInterpolator.getEntries()[index]);
             return true;
+        } else if (preference == mCustomWallpaper) {
+            int selection = mCustomWallpaper.findIndexOfValue(objValue.toString());
+            return handleWallpaperSelection(selection);
         }
         return false;
+    }
+
+    private boolean handleWallpaperSelection(int selection) {
+         if (selection == LOCKSCREEN_WALLPAPER_CUSTOM) {
+            final Intent intent = new Intent(Intent.ACTION_GET_CONTENT, null);
+            intent.setType("image/*");
+            intent.putExtra("crop", "true");
+            intent.putExtra("scale", true);
+            intent.putExtra("scaleUpIfNeeded", false);
+            intent.putExtra("outputFormat", Bitmap.CompressFormat.PNG.toString());
+
+            final Display display = getActivity().getWindowManager().getDefaultDisplay();
+            final Rect rect = new Rect();
+            final Window window = getActivity().getWindow();
+
+            window.getDecorView().getWindowVisibleDisplayFrame(rect);
+
+            int statusBarHeight = rect.top;
+            int contentViewTop = window.findViewById(Window.ID_ANDROID_CONTENT).getTop();
+            int titleBarHeight = contentViewTop - statusBarHeight;
+            boolean isPortrait = getResources().getConfiguration().orientation ==
+                    Configuration.ORIENTATION_PORTRAIT;
+
+            int width = display.getWidth();
+            int height = display.getHeight() - titleBarHeight;
+
+            intent.putExtra("aspectX", isPortrait ? width : height);
+            intent.putExtra("aspectY", isPortrait ? height : width);
+
+            try {
+                mWallpaperTemporary.createNewFile();
+                mWallpaperTemporary.setWritable(true, false);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(mWallpaperTemporary));
+                intent.putExtra("return-data", false);
+                getActivity().startActivityFromFragment(this, intent, REQUEST_CODE_BG_WALLPAPER);
+            } catch (IOException e) {
+                // Do nothing here
+            } catch (ActivityNotFoundException e) {
+                // Do nothing here
+            }
+        } else if (selection == LOCKSCREEN_DEFAULT_WALLPAPER) {
+                if (mWallpaperImage.exists()) {
+                    mWallpaperImage.delete();
+                }
+                updateCustomWallpaperSummary();
+        }
+
+        return false;
+    }
+
+    private boolean lockWallpaperExists() {
+        File wallpaper = new File(WALLPAPER_IMAGE_PATH + File.separator + WALLPAPER_FILE_NAME);
+        return wallpaper.exists();
     }
 }
