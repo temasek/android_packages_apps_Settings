f284330ad95178c44a4235a831d0ae11e029f6b3
Janson Kang <temasek71@gmail.com>
2014-04-13 07:49:49 +0800

    Revert "QS: add theme switcher tile (TRDS) 2/2"
    This reverts commit f22478e40b03d3fa018146469c7bbd5a7e58cddc.
    
    Conflicts:
    
    	res/values/cr_strings.xml
    	src/com/android/settings/quicksettings/QuickSettingsUtil.java

--- src/com/android/settings/quicksettings/QuickSettingsUtil.java
+++ src/com/android/settings/quicksettings/QuickSettingsUtil.java
@@ -46,8 +46,7 @@ import static com.android.internal.util.cm.QSConstants.TILE_USER;
 import static com.android.internal.util.cm.QSConstants.TILE_VOLUME;
 import static com.android.internal.util.cm.QSConstants.TILE_WIFI;
 import static com.android.internal.util.cm.QSConstants.TILE_WIFIAP;
-import static com.android.internal.util.cm.QSConstants.TILE_POWER;
-import static com.android.internal.util.cm.QSConstants.TILE_THEME;  
+import static com.android.internal.util.cm.QSConstants.TILE_POWER;  
 
 import android.content.ContentResolver;
 import android.content.Context;
@@ -159,9 +158,6 @@ public class QuickSettingsUtil {
 	registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_POWER, R.string.title_tile_power,
                 "com.android.systemui:drawable/ic_qs_powermenu"));
-        registerTile(new QuickSettingsUtil.TileInfo(
-                TILE_THEME, R.string.title_tile_theme,
-                "com.android.systemui:drawable/ic_qs_theme_manual"));    
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_ONTHEGO, R.string.title_tile_onthego,
                 "com.android.systemui:drawable/ic_qs_onthego"));
